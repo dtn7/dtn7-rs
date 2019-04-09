548b2c12679e8d786d10122abd455305f349a441
Lars Baumgaertner <baumgaertner@cs.tu-darmstadt.de>
2019-04-10 01:55:23 +0200

    refactored core::core to core and added routingagent to global config struct

--- src/core/helpers.rs
+++ src/core/helpers.rs
@@ -1,21 +1,25 @@
+use super::*;
+use bp7::EndpointID;
+use rand::distributions::Alphanumeric;
+use rand::thread_rng;
 use rand::Rng;
 use std::net::IpAddr;
-use crate::core::*;
 
-
-pub fn rnd_peer() -> core::DtnPeer {
+pub fn rnd_peer() -> DtnPeer {
     let peertype = match rand::thread_rng().gen_range(0, 2) {
-        0 => core::PeerType::Static,
-        _ => core::PeerType::Dynamic,
+        0 => PeerType::Static,
+        _ => PeerType::Dynamic,
     };
+    let rstr: String = thread_rng().sample_iter(&Alphanumeric).take(10).collect();
+    let eid = EndpointID::from(format!("dtn://{}", rstr));
     match rand::thread_rng().gen_range(0, 2) {
         0 => {
             let random_bytes = rand::thread_rng().gen::<[u8; 4]>();
-            core::DtnPeer::new(None, IpAddr::from(random_bytes), peertype, Vec::new())
+            DtnPeer::new(eid, IpAddr::from(random_bytes), peertype, Vec::new())
         }
         _ => {
             let random_bytes = rand::thread_rng().gen::<[u8; 16]>();
-            core::DtnPeer::new(None, IpAddr::from(random_bytes), peertype, Vec::new())
+            DtnPeer::new(eid, IpAddr::from(random_bytes), peertype, Vec::new())
         }
     }
 }
