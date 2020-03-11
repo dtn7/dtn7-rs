3480c441e2d53f31c91a869e5db8a56567069681
Lars Baumgaertner <1264131+gh0st42@users.noreply.github.com>
2020-03-11 21:01:02 +0100

    some cleanup and new real world test cases, version 0.8.5
    - bumped minimum bp7 version to 0.5.3, required for `.endpoint()` in `EndpointID`
    - split `tests/local_nodes.sh` into two script, one for dtn naming and the other for ipn naming
    - local_nodes test script now also aborts if compilation fails

---
+++ tests/local_nodes_dtn.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cargo build --bins
+
+if [ $? -ne 0 ]
+then
+  echo "Build failed."
+  exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+OUT_NODE1=$(mktemp /tmp/node1.XXXXXX)
+PORT_NODE1=3000
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE1 -C mtcp:2342 -e incoming -r epidemic -n node1 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE1 &
+PID_NODE1=$!
+echo node1 pid: $PID_NODE1
+echo node1 out: $OUT_NODE1
+echo node1 port: $PORT_NODE1
+
+
+OUT_NODE2=$(mktemp /tmp/node2.XXXXXX)
+PORT_NODE2=3001
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE2 -C mtcp:4223 -e incoming -r epidemic \
+    -n node2 \
+    -s mtcp://127.0.0.1:2342/node1 \
+    -s mtcp://127.0.0.1:2432/node3 \
+    2>&1 &> $OUT_NODE2 &
+PID_NODE2=$!
+echo node2 pid: $PID_NODE2
+echo node2 out: $OUT_NODE2
+echo node2 port: $PORT_NODE2
+
+OUT_NODE3=$(mktemp /tmp/node3.XXXXXX)
+PORT_NODE3=3002
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE3 -C mtcp:2432 -e incoming -r epidemic -n node3 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE3 &
+PID_NODE3=$!
+echo node3 pid: $PID_NODE3
+echo node3 out: $OUT_NODE3
+echo node3 port: $PORT_NODE3
+
+sleep 1
+
+echo
+
+echo "Sending 'test' to node 3"
+echo test | $DIR/../target/debug/dtnsend -r dtn://node3/incoming -p $PORT_NODE1
+
+sleep 1
+
+echo -n "Receiving on node 3: "
+$DIR/../target/debug/dtnrecv -v -e incoming -p $PORT_NODE3
+echo 
+
+echo "Press any key to stop daemons and clean up logs"
+read -n 1
+kill $PID_NODE1 $PID_NODE2 $PID_NODE3
+rm $OUT_NODE1 $OUT_NODE2 $OUT_NODE3
--- tests/local_nodes_ipn.sh
+++ tests/local_nodes_ipn.sh
@@ -2,11 +2,17 @@
 
 cargo build --bins
 
+if [ $? -ne 0 ]
+then
+  echo "Build failed."
+  exit 1
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 OUT_NODE1=$(mktemp /tmp/node1.XXXXXX)
 PORT_NODE1=3000
-$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE1 -C mtcp:2342 -e incoming -r epidemic -n node1 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE1 &
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE1 -C mtcp:2342 -e 42 -r epidemic -n 1 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE1 &
 PID_NODE1=$!
 echo node1 pid: $PID_NODE1
 echo node1 out: $OUT_NODE1
@@ -15,8 +21,8 @@ echo node1 port: $PORT_NODE1
 
 OUT_NODE2=$(mktemp /tmp/node2.XXXXXX)
 PORT_NODE2=3001
-$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE2 -C mtcp:4223 -e incoming -r epidemic \
-    -n node2 \
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE2 -C mtcp:4223 -e 42 -r epidemic \
+    -n 2 \
     -s mtcp://127.0.0.1:2342/node1 \
     -s mtcp://127.0.0.1:2432/node3 \
     2>&1 &> $OUT_NODE2 &
@@ -27,7 +33,7 @@ echo node2 port: $PORT_NODE2
 
 OUT_NODE3=$(mktemp /tmp/node3.XXXXXX)
 PORT_NODE3=3002
-$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE3 -C mtcp:2432 -e incoming -r epidemic -n node3 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE3 &
+$DIR/../target/debug/dtnd -d -j0 -i0 -w $PORT_NODE3 -C mtcp:2432 -e 42 -r epidemic -n 3 -s mtcp://127.0.0.1:4223/node2 2>&1 &> $OUT_NODE3 &
 PID_NODE3=$!
 echo node3 pid: $PID_NODE3
 echo node3 out: $OUT_NODE3
@@ -37,13 +43,13 @@ sleep 1
 
 echo
 
-echo "Sending 'test' to node 3"
-echo test | $DIR/../target/debug/dtnsend -r dtn://node3/incoming -p $PORT_NODE1
+echo "Sending 'test' to ipn://3.42"
+echo test | $DIR/../target/debug/dtnsend -r ipn://3.42 -p $PORT_NODE1
 
 sleep 1
 
 echo -n "Receiving on node 3: "
-$DIR/../target/debug/dtnrecv -v -e incoming -p $PORT_NODE3
+$DIR/../target/debug/dtnrecv -v -e 42 -p $PORT_NODE3
 echo 
 
 echo "Press any key to stop daemons and clean up logs"
