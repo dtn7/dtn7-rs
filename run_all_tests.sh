eef9a789230addde4e8b61ca4c616035bf56a54f
gh0st <1264131+gh0st42@users.noreply.github.com>
2021-10-13 14:21:00 +0200

    test: added test for non-singleton group communication

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,6 +4,7 @@ cargo test --release && \
 ./tests/local_nodes_dtn.sh && \
 ./tests/local_nodes_ipn.sh && \
 ./tests/local_ping_echo.sh && \
+./tests/local_group_test.sh && \
 ./tests/local_trigger_test.sh && \
 ./tests/cla_chain_test.sh && \
 echo "SUCCESS"
