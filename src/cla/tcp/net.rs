686c5f9b0dc689eea7f2389f2f60a94cc14d611c
lehmanju <git@devpi.de>
2021-12-02 16:03:14 +0100

    feat(tcp): add bundle retransmission prevention extension to TCPCL (#8)

--- src/cla/tcp/net.rs
+++ src/cla/tcp/net.rs
@@ -1,8 +1,12 @@
+use std::io::Cursor;
+
 use super::proto::*;
 use log::{debug, error, warn};
 use num_traits::FromPrimitive;
 use thiserror::Error;
 use tokio::io::{self, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+
+const MINIMUM_EXTENSION_ITEM_SIZE: u32 = 5;
 
 #[derive(Debug, Eq, PartialEq)]
 pub(crate) enum TcpClPacket {
@@ -40,10 +44,25 @@ impl TcpClPacket {
                 writer.write_u8(xfer_seg_data.flags.bits()).await?;
                 writer.write_u64(xfer_seg_data.tid).await?;
                 if xfer_seg_data.flags.contains(XferSegmentFlags::START) {
-                    writer.write_u32(0).await?;
+                    let mut ext_data = Cursor::new(Vec::new());
+                    let mut len = 0u32;
+                    for ext in &xfer_seg_data.extensions {
+                        ext_data.write_u8(ext.flags.bits()).await.unwrap();
+                        ext_data.write_u16(ext.item_type as u16).await.unwrap();
+                        ext_data.write_u16(ext.data.len() as u16).await.unwrap();
+                        ext_data.write_all(ext.data.as_ref()).await?;
+                        len += MINIMUM_EXTENSION_ITEM_SIZE + ext.data.len() as u32;
+                    }
+                    writer.write_u32(len).await?;
+                    if len > 0 {
+                        writer.write_all(ext_data.get_ref()).await?;
+                    }
                 }
                 writer.write_u64(xfer_seg_data.len).await?;
-                writer.write_all(xfer_seg_data.buf.as_ref()).await?;
+
+                if xfer_seg_data.len > 0 {
+                    writer.write_all(xfer_seg_data.buf.as_ref()).await?;
+                }
             }
             TcpClPacket::XferAck(xfer_ack_data) => {
                 writer.write_u8(MessageType::XferAck as u8).await?;
@@ -75,26 +94,52 @@ impl TcpClPacket {
                 MessageType::XferSegment => {
                     let flags = XferSegmentFlags::from_bits_truncate(reader.read_u8().await?);
                     let tid: u64 = reader.read_u64().await?;
+                    let mut extensions = Vec::new();
                     if flags.contains(XferSegmentFlags::START) {
-                        let ext_len: u32 = reader.read_u32().await?;
+                        let mut ext_len: u32 = reader.read_u32().await?;
+                        // parse bundle ids that are request
                         if ext_len != 0 {
-                            warn!("transfer extension are unsupported, ignoring them!");
-                        }
-                        for _ in 0..ext_len {
-                            reader.read_u8().await?;
+                            debug!("parsing transfer extensions");
+                            while ext_len >= MINIMUM_EXTENSION_ITEM_SIZE {
+                                let flag = reader.read_u8().await?;
+                                let item_type = reader.read_u16().await?;
+                                let item_length = reader.read_u16().await?;
+                                ext_len =
+                                    ext_len - MINIMUM_EXTENSION_ITEM_SIZE - item_length as u32;
+                                let mut data = vec![0; item_length as usize];
+                                reader.read_exact(&mut data).await?;
+                                if let Some(item_type) =
+                                    TransferExtensionItemType::from_u16(item_type)
+                                {
+                                    let transfer_extension = TransferExtensionItem {
+                                        flags: TransferExtensionItemFlags::from_bits_truncate(flag),
+                                        item_type,
+                                        data: data.into(),
+                                    };
+                                    extensions.push(transfer_extension);
+                                }
+                            }
+                            if ext_len != 0 {
+                                warn!("malformed transfer extensions, ignoring rest");
+                                for _ in 0..ext_len {
+                                    reader.read_u8().await?;
+                                }
+                            }
                         }
                     }
                     let len = reader.read_u64().await?;
                     debug!("Reading xfer segment with len {}", len);
-                    let mut data = Vec::with_capacity(len as usize);
-                    data.resize_with(len as usize, Default::default);
-                    reader.read_exact(&mut data).await?;
+                    let mut data = vec![0; len as usize];
+                    if len > 0 {
+                        reader.read_exact(&mut data).await?;
+                    }
 
                     let seg = XferSegData {
                         flags,
                         tid,
                         len,
                         buf: data.into(),
+                        extensions,
                     };
 
                     Ok(TcpClPacket::XferSeg(seg))
