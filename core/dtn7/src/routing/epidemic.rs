211755def69fbadcc9e2fd9eb9873c5832ff19c1
Daniel Schmidt <info@daniel-schmidt.me>
2022-03-23 19:50:47 +0100

    EpidemicRoutingAgentCore struct instead of direct history access.

--- core/dtn7/src/routing/epidemic.rs
+++ core/dtn7/src/routing/epidemic.rs
@@ -20,62 +20,63 @@ impl Default for EpidemicRoutingAgent {
     }
 }
 
-fn add(history: &mut HashMap<String, HashSet<String>>, bundle_id: String, node_name: String) {
-    let entries = history.entry(bundle_id).or_insert_with(HashSet::new);
-    entries.insert(node_name);
+/// Keeps history about which bundles were already sent to what peers.
+struct EpidemicRoutingAgentCore {
+    history: HashMap<String, HashSet<String>>,
 }
 
-/*fn remove_bundle(&mut self, bundle_id: String) {
-    self.history.remove(&bundle_id);
-}*/
-
-/*fn filtered(&mut self, dest: String, bundles: &[ByteBuffer]) -> Vec<ByteBuffer> {
-    let entries = self.history.entry(dest).or_insert_with(HashSet::new);
-    bundles
-        .iter()
-        .cloned()
-        .filter(|b| !entries.contains(b))
-        .collect()
-}*/
-
-fn contains(
-    history: &mut HashMap<String, HashSet<String>>,
-    bundle_id: &str,
-    node_name: &str,
-) -> bool {
-    if let Some(entries) = history.get(bundle_id) {
-        //let entries = self.history.entry(bundle_id);
-        return entries.contains(node_name);
+impl EpidemicRoutingAgentCore {
+    pub fn new() -> EpidemicRoutingAgentCore {
+        EpidemicRoutingAgentCore {
+            history: HashMap::new(),
+        }
     }
-    false
-}
 
-fn sending_failed(
-    history: &mut HashMap<String, HashSet<String>>,
-    bundle_id: &str,
-    node_name: &str,
-) {
-    if let Some(entries) = history.get_mut(bundle_id) {
-        entries.remove(node_name);
-        debug!(
-            "removed {:?} from sent list for bundle {}",
-            node_name, bundle_id
-        );
+    fn add(&mut self, bundle_id: String, node_name: String) {
+        let entries = self.history.entry(bundle_id).or_insert_with(HashSet::new);
+        entries.insert(node_name);
     }
-}
 
-fn incoming_bundle(
-    history: &mut HashMap<String, HashSet<String>>,
-    bundle_id: &str,
-    node_name: &str,
-) {
-    if !node_name.is_empty() && !contains(history, bundle_id, node_name) {
-        add(history, bundle_id.to_string(), node_name.to_string());
+    /*fn remove_bundle(&mut self, bundle_id: String) {
+        self.history.remove(&bundle_id);
+    }*/
+
+    /*fn filtered(&mut self, dest: String, bundles: &[ByteBuffer]) -> Vec<ByteBuffer> {
+        let entries = self.history.entry(dest).or_insert_with(HashSet::new);
+        bundles
+            .iter()
+            .cloned()
+            .filter(|b| !entries.contains(b))
+            .collect()
+    }*/
+
+    fn contains(&self, bundle_id: &str, node_name: &str) -> bool {
+        if let Some(entries) = self.history.get(bundle_id) {
+            //let entries = self.history.entry(bundle_id);
+            return entries.contains(node_name);
+        }
+        false
+    }
+
+    fn sending_failed(&mut self, bundle_id: &str, node_name: &str) {
+        if let Some(entries) = self.history.get_mut(bundle_id) {
+            entries.remove(node_name);
+            debug!(
+                "removed {:?} from sent list for bundle {}",
+                node_name, bundle_id
+            );
+        }
+    }
+
+    fn incoming_bundle(&mut self, bundle_id: &str, node_name: &str) {
+        if !node_name.is_empty() && !self.contains(bundle_id, node_name) {
+            self.add(bundle_id.to_string(), node_name.to_string());
+        }
     }
 }
 
 async fn sender_for_bundle(mut rx: mpsc::Receiver<RoutingCmd>) {
-    let mut history: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut core: EpidemicRoutingAgentCore = EpidemicRoutingAgentCore::new();
 
     while let Some(cmd) = rx.recv().await {
         match cmd {
@@ -83,9 +84,9 @@ async fn sender_for_bundle(mut rx: mpsc::Receiver<RoutingCmd>) {
                 let mut clas = Vec::new();
                 let mut delete_afterwards = false;
                 for (_, p) in (*PEERS.lock()).iter() {
-                    if !contains(&mut history, bp.id(), &p.node_name()) {
+                    if !core.contains(bp.id(), &p.node_name()) {
                         if let Some(cla) = p.first_cla() {
-                            add(&mut history, bp.id().to_string(), p.node_name().clone());
+                            core.add(bp.id().to_string(), p.node_name().clone());
                             if bp.destination.node().unwrap() == p.node_name() {
                                 // direct delivery possible
                                 debug!(
@@ -113,17 +114,17 @@ async fn sender_for_bundle(mut rx: mpsc::Receiver<RoutingCmd>) {
             }
             super::RoutingCmd::Notify(notification) => match notification {
                 RoutingNotifcation::SendingFailed(bid, cla_sender) => {
-                    sending_failed(&mut history, bid.as_str(), cla_sender.as_str());
+                    core.sending_failed(bid.as_str(), cla_sender.as_str());
                 }
                 RoutingNotifcation::IncomingBundle(bndl) => {
                     if let Some(eid) = bndl.previous_node() {
                         if let Some(node_name) = eid.node() {
-                            incoming_bundle(&mut history, &bndl.id(), &node_name);
+                            core.incoming_bundle(&bndl.id(), &node_name);
                         }
                     };
                 }
                 RoutingNotifcation::IncomingBundleWithoutPreviousNode(bid, node_name) => {
-                    incoming_bundle(&mut history, bid.as_str(), node_name.as_str());
+                    core.incoming_bundle(bid.as_str(), node_name.as_str());
                 }
                 _ => {}
             },
