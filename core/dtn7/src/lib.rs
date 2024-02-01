ca933bd2e989b0e86117e56f18715c3c80b98b0b
Thomas Schmitt <t.e.schmitt@posteo.de>
2024-02-01 21:52:16 +0100

    delete bundle from store

--- core/dtn7/src/lib.rs
+++ core/dtn7/src/lib.rs
@@ -180,10 +180,12 @@ pub fn store_add_bundle_if_unknown(bndl: &Bundle) -> Result<bool> {
 }
 
 pub fn store_remove(bid: &str) -> Result<()> {
-    info!("Removing bundle {}", bid);
+    info!("store_remove: Removing bundle {}", bid);
     if let Err(err) = (*STORE.lock()).remove(bid) {
         error!("store_remove: {}", err);
         return Err(err);
+    } else {
+        debug!("store_remove: Success!");
     }
     Ok(())
 }
