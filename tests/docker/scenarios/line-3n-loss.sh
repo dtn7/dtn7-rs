63cc9d2137742133105a0650040081a3d069c3c1
gh0st <1264131+gh0st42@users.noreply.github.com>
2024-03-08 23:10:37 +0100

    test(docker): made line-3n-loss.sh for docker compose scenario compatible with docker and podman

--- tests/docker/scenarios/line-3n-loss.sh
+++ tests/docker/scenarios/line-3n-loss.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+DOCKERCMD=docker
+# use podman if available
+if command -v podman &> /dev/null; then
+  DOCKERCMD=podman
+fi
+$DOCKERCMD exec line-3n-n1-1 tc qdisc add dev eth0 root netem loss 0%
+$DOCKERCMD exec line-3n-n2-1 tc qdisc add dev eth0 root netem loss 0%
+$DOCKERCMD exec line-3n-n3-1 tc qdisc add dev eth0 root netem loss 0%
+
+cleanup () {
+  echo "Cleaning up..."
+  $DOCKERCMD exec line-3n-n1-1 tc qdisc del dev eth0 root netem loss 0%
+  $DOCKERCMD exec line-3n-n2-1 tc qdisc del dev eth0 root netem loss 0%
+  $DOCKERCMD exec line-3n-n3-1 tc qdisc del dev eth0 root netem loss 0%
+}
+
+trap cleanup EXIT
+
+
 while true; do
   echo "Setting loss on n3 to 100% for 30s" 
-  docker run --rm  -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem --duration 30s loss -p 100 line-3n-n3-1 &
+  $DOCKERCMD exec line-3n-n3-1 tc qdisc change dev eth0 root netem loss 100%
   sleep 30
   echo " n3 back to 0% loss." 
+  $DOCKERCMD exec line-3n-n3-1 tc qdisc change dev eth0 root netem loss 0%
   echo "Setting loss on n1 to 100% for 30s"	
-  docker run --rm  -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba netem --duration 30s loss -p 100 line-3n-n1-1 &
+  $DOCKERCMD exec line-3n-n1-1 tc qdisc change dev eth0 root netem loss 100%
   sleep 30
   echo " n1 back to 0% loss."
+  $DOCKERCMD exec line-3n-n1-1 tc qdisc change dev eth0 root netem loss 0%
 done
